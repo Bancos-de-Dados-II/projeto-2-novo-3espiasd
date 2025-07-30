8c0ba1350565499ff90cfe44d7eebcb4ea258458
Darkiane <darkianelima13@gmail.com>
2025-07-30 11:05:20 -0300

    Implementando o CRUD no controller.

---
+++ src/controller/eventos-controller.ts
@@ -0,0 +1,96 @@
+import Evento from "../models/Evento.js"
+import { Request, Response } from 'express';
+
+type Params = {
+    id: string
+}
+
+type Paramentro = {
+    nome: string;
+    descricao?: string;
+    tipo: 'Cultural' | 'Social' | 'Religioso' | 'Recreativo' | 'Outro';
+    dataHora?: Date;
+    local: {
+        type: 'Point';
+        coordinates: [number, number]; // longitude, latitude
+    };
+    descricaoLocal?: string;
+    imagens: string[];
+}
+
+export async function getEventos(req: Request, res: Response) {
+    try {
+        const events = await Evento.find();
+        res.status(200).json(events);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao buscar eventos", error });
+    }
+}
+
+export async function getEventosById(req: Request, res: Response): Promise<void> {
+    try {
+        const { id } = req.params as Params;
+        const event = await Evento.findById(id);
+        if (!event) {
+            res.status(404).json({ message: "Evento não encontrado" });
+            return;
+        }
+        res.status(200).json(event);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao buscar evento", error });
+    }
+}
+
+export async function postEvento(req: Request, res: Response) {
+    try {
+        const { nome, descricao, tipo, dataHora, local, descricaoLocal, imagens } = req.body as Paramentro;
+        const newEvent = await Evento.create({
+            nome,
+            descricao,
+            tipo,
+            dataHora,
+            local,
+            descricaoLocal,
+            imagens
+        });
+        res.status(201).json(newEvent);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao criar o evento", error });
+    }
+}
+
+export async function putEvento(req: Request, res: Response): Promise<void> {
+    try {
+        const { id } = req.params as Params;
+        const event = await Evento.findById(id);
+
+        if (!event) {
+            res.status(404).json({ message: "Evento não encontrado" });
+            return;
+        }
+
+        event.set(req.body);
+        await event.save();
+
+        res.status(200).json(event);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({ message: "Falha ao atualizar", error: err });
+    }
+}
+
+export async function delEvento(req: Request, res: Response): Promise<void> {
+    try {
+        const { id } = req.params as Params;
+        const deleted = await Evento.findByIdAndDelete(id);
+
+        if (!deleted) {
+            res.status(404).json({ message: "Evento não encontrado" });
+            return;
+        }
+
+        res.json({ message: "Evento removido com sucesso" });
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao deletar evento", error });
+    }
+}
